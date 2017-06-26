55f641c2ebb19327c846c7600c77104c2ce300ef
Colin Walbridge <cwalbrid@umich.edu>
2017-06-26 04:52:01 -0400

    cleaned up, de-linted, added comments

--- app/js/app.js
+++ app/js/app.js
@@ -2,33 +2,6 @@ $(document).ready(function(){
 
 $('#my-slider').carousel();
 
-$('.h1-trails-copy').on('click', function(){
-	alert ('you clicked a plus sign!')
-})
-
-
-
-
-
-//Event 1: Nav-Left is clicked
-//the slide directly above the 
-//featured slide becomes the featured slide
-
-
-//Event 2: Nav-Right is clicked
-// the slide directly below the 
-//featured slide becomes the featured slide
-
-
-//Event 3: Info-btn hover 
-//element added next to info button that gives title of item
-
-//Event 4: Info-btn clicked
-//slide with number matching info-btn number becomes featured slide.
-
-
-
-
 
 
 ////
